e30f8345d8ad9637466e125d120298510fad4cfe
IssacRawCoreOfficiak <issacrawcore@gmail.com>
2024-08-23 10:44:43 -0300

    byedkicked.lua

--- byedkicked.lua
+++ byedkicked.lua
@@ -4,27 +4,34 @@ local frame = Instance.new("Frame")
 local textbox = Instance.new("TextBox")
 local kick = Instance.new("TextButton")
 local ban = Instance.new("TextButton")
+local minimize = Instance.new("TextButton") -- new button
 
 -- properties
-gui.Name = "Admin GUI"
+gui.Name = "BYEDKICKED GUI V1"
 frame.Size = UDim2.new(0, 200, 0, 150)
 frame.Position = UDim2.new(0.5, -100, 0.5, -75)
 frame.AnchorPoint = Vector2.new(0.5, 0.5)
+textbox.PlaceholderText = "Player"
 textbox.Position = UDim2.new(0, 10, 0, 10)
 textbox.Size = UDim2.new(0, 190, 0, 25)
 kick.Position = UDim2.new(0, 10, 0, 50)
 kick.Size = UDim2.new(0, 90, 0, 25)
 ban.Position = UDim2.new(0, 110, 0, 50)
 ban.Size = UDim2.new(0, 90, 0, 25)
+minimize.Position = UDim2.new(0, 170, 0, 10)
+minimize.Size = UDim2.new(0, 30, 0, 20)
 kick.Text = "Kick"
 ban.Text = "Ban Permanent"
+minimize.Text = "—"
 
 -- scripts
 kick.MouseButton1Click:Connect(function()
 -- kick player
 local player = game.Players:FindFirstChild(textbox.Text)
 if player then
-player:Kick("BYEDKICKED GUI BANNED YOU 😂")
+player:Kick("BYEDKICKED GUI KICKED YOU 😂")
+wait(1)
+player.Camera.CameraSubject = nil
 end
 end)
 
@@ -37,9 +44,15 @@ player:Kick("BYEDKICKED GUI BANNED YOU PERMANENTLY 😂")
 end
 end)
 
+minimize.MouseButton1Click:Connect(function()
+-- minimize gui
+frame.Size = UDim2.new(0, 30, 0, 20)
+end)
+
 -- parent
 gui.Parent = game.Players.LocalPlayer.PlayerGui
 frame.Parent = gui
 textbox.Parent = frame
 kick.Parent = frame
 ban.Parent = frame
+minimize.Parent = frame
